9007c34d3b788c5d8c4624d057294dc6f0b366aa
umt3 <47469383+umt3@users.noreply.github.com>
2019-09-23 23:12:43 -0400

    Add files via upload

---
+++ Assets/playerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+        }
+
+    }
+    void SetCountText()
+    {
+        //countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+        }
+
+    }
+}
